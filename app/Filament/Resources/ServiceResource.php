e7cee67f8848d964451e246ffd81444b24ded8dc
ramaaudra <ramaaudra30.ra@gmail.com>
2024-09-02 12:19:21 +0700

    end

--- app/Filament/Resources/ServiceResource.php
+++ app/Filament/Resources/ServiceResource.php
@@ -67,7 +67,7 @@ class ServiceResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-            Tables\Actions\DeleteAction::make()
+                Tables\Actions\DeleteAction::make()
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
